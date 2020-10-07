2d18d0a6297820d4e0723d69ebe4dd022560192e
NinjaTxas <NinjaTxas@users.noreply.github.com>
2020-10-07 14:51:33 +0200

    user2.py
    hhellow whats u[

---
+++ user2.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+#HEELLO EHLLLo
+
+class User(object):
+
+	expirationDate = datetime(1990, 1, 1)
+	lastFreeCoffeeDate = datetime(1990, 1, 1)
+
+	def isActiveMember(self):
+		if self.expiration > datetime.now():
+			return True
+		return False
+
+	def isFreeCoffee(self):
+		if self.isActiveMember() == False:
+			return False
+		else:
+			now = datetime.now()
+			if now.year > self.lastFreeCoffeeDate.year:
+				return True
+			elif now.month > self.lastFreeCoffeeDate.month:
+				return True
+			elif now.day > self.lastFreeCoffeeDate.day:
+				return True
+			else:
+				return False
+
+
+user = User()
+user.expiration = datetime(2020, 10, 10)
+user.lastFreeCoffeeDate = datetime(2020, 10, 7)
+
+print(user.isFreeCoffee())
