80ee6d4d195f97050fda58c63959f6e527065932
NinjaTxas <NinjaTxas@users.noreply.github.com>
2020-10-08 15:01:11 +0200

    Update with init

--- user.py
+++ user.py
@@ -1,14 +1,27 @@
-from datetime import datetime
-#helloow
-#HEELLO EHLLLo
-#Heyyy
+from datetime import date
+
 class User(object):
+
+	# Probably the following is redundant but cooming from Java
+	# 	I feel like its handy to have a list of object
+	#	variables at the top of the class
+	name = None
+	surname = None
+	id = None
+	cardKey = None
+	# ---
 
 	expirationDate = datetime(1990, 1, 1)
 	lastFreeCoffeeDate = datetime(1990, 1, 1)
 
+	def __init__(self, id, cardKey, name = "Unknown", surname = "Unknown"):
+		self.id = id # TODO implement auto ID assignemnt using database connection.
+		self.cardKey = cardKey
+		self.name = name
+		self.surname = surname
+
 	def isActiveMember(self):
-		if self.expiration > datetime.now():
+		if self.expiration > date.now(): # Checks if expiration date is later than today.
 			return True
 		return False
 
@@ -16,7 +29,7 @@ class User(object):
 		if self.isActiveMember() == False:
 			return False
 		else:
-			now = datetime.now()
+			now = date.now()
 			if now.year > self.lastFreeCoffeeDate.year:
 				return True
 			elif now.month > self.lastFreeCoffeeDate.month:
@@ -28,7 +41,7 @@ class User(object):
 
 
 user = User()
-user.expiration = datetime(2020, 10, 10)
-user.lastFreeCoffeeDate = datetime(2020, 10, 7)
-
+user.expiration = date(2020, 10, 10)
+user.lastFreeCoffeeDate = date(2020, 10, 7)
+print(date.today())
 print(user.isFreeCoffee())
