127cae0034a93dd74b66c90cdcd7bc34996209d7
Bantosito <72502994+Bantosito@users.noreply.github.com>
2020-10-08 15:46:39 +0200

    Airidas version of user. The one to begin with.

--- user.py
+++ user.py
@@ -1,11 +1,25 @@
-from datetime import datetime
+from datetime import date
+
 class User(object):
 
-	expirationDate = datetime(1990, 1, 1)
-	lastFreeCoffeeDate = datetime(1990, 1, 1)
+	# Probably the following is redundant but cooming from Java
+	# 	I feel like its handy to have a list of object
+	#	variables at the top of the class
+	name = None
+	surname = None
+	cardKey = None
+	# ---
+
+	expirationDate = date(1990, 1, 1)
+	lastFreeCoffeeDate = date(1990, 1, 1)
+
+	def __init__(self, cardKey, name = "Unknown", surname = "Unknown"):
+		self.cardKey = cardKey
+		self.name = name
+		self.surname = surname
 
 	def isActiveMember(self):
-		if self.expiration > datetime.now():
+		if self.expiration > date.today(): # Checks if expiration date is later than today.
 			return True
 		return False
 
@@ -13,7 +27,7 @@ class User(object):
 		if self.isActiveMember() == False:
 			return False
 		else:
-			now = datetime.now()
+			now = date.today()
 			if now.year > self.lastFreeCoffeeDate.year:
 				return True
 			elif now.month > self.lastFreeCoffeeDate.month:
@@ -24,8 +38,8 @@ class User(object):
 				return False
 
 
-user = User()
-user.expiration = datetime(2020, 10, 10)
-user.lastFreeCoffeeDate = datetime(2020, 10, 7)
-
-print(user.isFreeCoffee())
+# user = User(1001, "testId")
+# user.expiration = date(2020, 10, 10)
+# user.lastFreeCoffeeDate = date(2020, 10, 7)
+# print(date.today())
+# print(user.isFreeCoffee())
