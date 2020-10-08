5ee2c518a9451c51b6a6e7fe2be7db8c49be3774
NinjaTxas <NinjaTxas@users.noreply.github.com>
2020-10-08 13:49:42 +0200

    Order py update

---
+++ order.py
@@ -0,0 +1,36 @@
+# order.py
+
+from datetime import date
+
+class Order(object):
+##################### class start
+     ####
+     # Initialization method.
+     # It also assigns default values for date and revenue.
+     # Create a new obejct of class Order by:
+     # myVariable = Order(date(2020, 10, 1), 4.50)
+     # The above will create an order at 2020-10-01 for 4.50 kr.
+     # Access the new order variables by:
+     # orderDate = myVariable.date
+     # orderRevenue = myVariable.revenue
+     ###
+     def __init__(self, date = date(1990,1,1), revenue = 0):
+         self.date = date
+         self.revenue = revenue
+
+##################### class end
+
+# List of orders. This is your Fake data
+allOrders = [
+Order(date(2020,10,1), 3.50),
+Order(date(2020,10,1), 2.95),
+Order(date(2020,10,2), 2.50),
+Order(date(2020,10,5), 1.50),
+Order(date(2020,10,5), 2.95),
+Order(date(2020,10,5), 7.95),
+]
+
+
+oneOrder = Order()
+print(oneOrder.revenue)
+print(oneOrder.date)
