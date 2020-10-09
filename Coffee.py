56dd90e800bc47391538934137fea54d897aa3da
NinjaTxas <NinjaTxas@users.noreply.github.com>
2020-10-09 12:08:13 +0200

    Dithched the size attr. Added Price.

--- Coffee.py
+++ Coffee.py
@@ -1,1 +1,9 @@
 # Coffee.py
+
+class Coffee(object):
+
+    def __init__(self, nameId, label = "Unknown", price = 0.00):
+        self.nameId = nameId
+        self.label = label
+        self.size = size
+        self.price = price
